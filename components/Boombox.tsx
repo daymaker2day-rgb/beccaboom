93973739767abd7fcffd136ac2da84a2671a6c63
Rebecca Media Player <dev@rebecca.local>
2025-11-01 06:59:59 -0700

    Fix Google Drive storage: Correct OAuth URLs and add Google Login button to main interface

--- components/Boombox.tsx
+++ components/Boombox.tsx
@@ -9,6 +9,7 @@ import { useVideos } from '../services/videoService';
 import { TapeState, RadioMode, MediaQueueItem } from '../types';
 import VideoControls from './VideoControls';
 import SettingsModal from './SettingsModal';
+import GoogleLogin from './GoogleLogin';
 
 const ThemeMenu: React.FC<{ onSelectTheme: (theme: string) => void }> = ({ onSelectTheme }) => {
   const themes = [
@@ -1036,6 +1037,11 @@ const Boombox: React.FC = () => {
                   <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.962.062 2.18-.948 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
                 </svg>
               </button>
+
+              {/* Google Drive Login */}
+              <div className="absolute top-1/2 -translate-y-1/2 right-20">
+                <GoogleLogin />
+              </div>
 
               {/* Profile Circle - moved inward with R logo option */}
               <button
